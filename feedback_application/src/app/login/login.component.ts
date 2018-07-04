7d863d38a8d876160d7758574fd124d8e34b4bdf
jacobmeyerson <jacobmeyerson@college.harvard.edu>
2018-07-04 14:48:28 +0300

    rudimentary authentication works

--- feedback_application/src/app/login/login.component.ts
+++ feedback_application/src/app/login/login.component.ts
@@ -1,21 +1,47 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  // loggedIn = false;
+export class LoginComponent {
   @Output() loggedInComplete = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(private http: Http) { }
 
   onLogin(username, password) {
     this.loggedInComplete.emit(true);
     console.log(username.value, password.value);
+
+    const headers = new Headers();
+
+    const base64 = btoa('jmeyerson' + ':' + 'Ampath123');
+
+    headers.append('Authorization', 'Basic ' + base64);
+
+    const url = 'https://ngx.ampath.or.ke/test-amrs/ws/rest/v1/session/'; // this.getUrl();
+
+    const request = this.http.get(url, {
+      headers: headers
+    });
+
+    request
+    .subscribe(
+      (response: Response) => {
+        console.log(response.text());
+        const data = response.json();
+        if (data.authenticated) {
+          console.log('authenticated');
+      }
+    });
   }
 }
+// OPENMRS_REST_SUFFIX = 'ws/rest/v1/'
+
+// if (this.getOpenmrsServer().endsWith('/')) {
+//   return this.getOpenmrsServer() + AppSettingsService.OPENMRS_REST_SUFFIX;
+
+
+// 'https://ngx.ampath.or.ke/test-amrs/'
